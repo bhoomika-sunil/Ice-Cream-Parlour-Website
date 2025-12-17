573102bd0d075c4fe19fcc8091d67e00210c0f15
bhoomika-sunil <bhoomikasunilkumar73@gmail.com>
2025-12-17 20:00:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function showFlavours() {
+    document.getElementById("output").innerHTML = `
+        <h2>Flavours and Price</h2>
+        <ul>
+            <li>Vanilla - 50rs</li>
+            <li>Chocolate - 60rs</li>
+            <li>Mango - 100rs</li>
+            <li>Pista - 80rs</li>
+        </ul>
+    `;
+}
+
+function showPrices() {
+    document.getElementById("output").innerHTML = `
+        <h2>Price List</h2>
+        <ul>
+            <li>Vanilla - 50rs</li>
+            <li>Chocolate - 60rs</li>
+            <li>Mango - 100rs</li>
+            <li>Pista - 80rs</li>
+        </ul>
+    `;
+}
+function showAmbience() {
+    document.getElementById("output").innerHTML = `
+        <div class="ambience">
+            <h2>Ambience</h2>
+            <p>Cozy seating, pastel interiors, soft lighting.</p>
+        </div>
+    `;
+}
+
+function showLocation() {
+    document.getElementById("output").innerHTML = `
+        <div class="location">
+            <h2>Location</h2>
+            <p>Sweet Scoop Ice-Cream Parlour, MG Road, Bengaluru.</p>
+        </div>
+    `;
+}
